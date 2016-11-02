77f35a67b256a1ce2d578d4ffb5d60686ae4e577
Jeff Pang <jeff.tspang@gmail.com>
2016-11-02 15:24:25 +0800

    initialise

---
+++ AppveyorTestBuild/Program.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppveyorTestBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                   .MinimumLevel.Debug()
+                   .WriteTo.LiterateConsole()
+                   .WriteTo.RollingFile("logs\\test-{Date}.log")
+                   .CreateLogger();
+
+            Log.Information("Test");
+        }
+    }
+}
